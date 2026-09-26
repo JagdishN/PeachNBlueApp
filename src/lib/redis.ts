58a09c7058dc0c1e54fdc7967e044cd09b685464
JagdishN <jagdish284@gmail.com>
2026-09-26 12:29:20 +0530

    Explicitly enable TLS for ioredis when using rediss:// (Upstash fix)

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,6 +4,7 @@ import { REDIS_URL } from '../config';
 const redis = new Redis(REDIS_URL, {
   maxRetriesPerRequest: 3,
   lazyConnect: true,
+  tls: REDIS_URL.startsWith('rediss://') ? {} : undefined,
 });
 
 redis.on('error', (err) => {
